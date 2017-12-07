6f76eb90ea37b09e25356189dc3cee0be646441e
Andrus <andrusjakobson@hotmail.com>
2017-12-07 15:45:49 +0200

    Andrus Jakobson: Finished APIClient and service.

--- AndroidStudioProjects/projectNAM2/app/src/main/java/com/example/andrus/projectnam/util/http/APIClient.java
+++ AndroidStudioProjects/projectNAM2/app/src/main/java/com/example/andrus/projectnam/util/http/APIClient.java
@@ -5,8 +5,27 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIClient {
-    Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("http://www.mocky.io/v2/")
-            .addConverterFactory(new GsonConverterFactory())
-            .build();
+    private static APIService service;
+    private static APIClient instance;
+
+    private APIClient() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://www.mocky.io/v2/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        service = retrofit.create(APIService.class);
+    }
+
+    public static APIClient getInstance() {
+        if (instance == null) {
+            instance = new APIClient();
+        }
+
+        return instance;
+    }
+
+    public APIService getService() {
+        return service;
+    }
 }
