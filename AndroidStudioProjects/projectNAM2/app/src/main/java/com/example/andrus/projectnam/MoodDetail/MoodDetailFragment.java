4eade6264a9c4db3a87112d2da0d3d62cfab37b7
Andrus <andrusjakobson@hotmail.com>
2017-12-07 10:18:40 +0200

    Andrus Jakobson: Removed Nullable from MoodDetailFragment

--- AndroidStudioProjects/projectNAM2/app/src/main/java/com/example/andrus/projectnam/MoodDetail/MoodDetailFragment.java
+++ AndroidStudioProjects/projectNAM2/app/src/main/java/com/example/andrus/projectnam/MoodDetail/MoodDetailFragment.java
@@ -1,7 +1,6 @@
 package com.example.andrus.projectnam.MoodDetail;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,15 +19,14 @@ public class MoodDetailFragment extends Fragment {
         return fragment;
     }
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         return inflater.inflate(R.layout.fragment_detail_thirsty, container, false);
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
     }
