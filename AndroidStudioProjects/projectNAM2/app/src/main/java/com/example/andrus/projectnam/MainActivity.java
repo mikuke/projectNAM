5800370d69ad10eea6321ba93a3a2c34a67210ab
AndrusJakobson <34274182+AndrusJakobson@users.noreply.github.com>
2017-12-13 12:40:34 +0200

    Mistake
    Mistake from one of the past merges which kept their conflicts.

--- AndroidStudioProjects/projectNAM2/app/src/main/java/com/example/andrus/projectnam/MainActivity.java
+++ AndroidStudioProjects/projectNAM2/app/src/main/java/com/example/andrus/projectnam/MainActivity.java
@@ -1,27 +1,18 @@
 package com.example.andrus.projectnam;
 
-<<<<<<< HEAD
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-=======
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.andrus.projectnam.MoodGrid.MoodGridFragment;
+import com.example.andrus.projectnam.moodgrid.MoodGridFragment;
 
->>>>>>> detail_thirsty
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-        //stuff
-=======
 
         showMainScreenFragment();
     }
@@ -34,6 +25,5 @@ public class MainActivity extends AppCompatActivity {
         transaction.replace(R.id.mainActivity_frameLayout, moodGridFragment);
 
         transaction.commit();
->>>>>>> detail_thirsty
     }
 }
